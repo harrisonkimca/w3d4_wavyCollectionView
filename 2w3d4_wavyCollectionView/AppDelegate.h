1049ea124f850768d1ff4d7893fc572781548341
hkimca <harrison.kim.ca@gmail.com>
2017-07-16 12:51:21 -0400

    Initial Commit

---
+++ 2w3d4_wavyCollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2w3d4_wavyCollectionView
+//
+//  Created by Seantastic31 on 16/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 2w3d4_wavyCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  2w3d4_wavyCollectionView
+//
+//  Created by Seantastic31 on 16/07/2017.
+//  Copyright © 2017 Seantastic31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
